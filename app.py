e49a5f1115851f5089878ec2d7dfd8b682a6336f
Christian Glissov <cdglissov@gmail.com>
2023-10-11 11:06:25 +0200

    draft

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
-def app() -> None:
-    print("Hello, World!")
+from src.myai.interface import gui
+
+
+def app():
+    gui.chat_gui()
+
+
+if __name__ == "__main__":
+    app()
---
+++ src/myai/interface/gui.py
@@ -0,0 +1,36 @@
+import streamlit as st
+
+
+def chat_gui():
+    st.set_page_config(
+        page_title="Hello",
+        page_icon="👋",
+    )
+
+    st.title("Chat UI")
+
+    st.markdown("Chat with me, human!")
+
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+
+    for message in st.session_state.messages:
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
+
+    # React to user input
+    if prompt := st.chat_input("What is up?"):
+        # Display user message in chat message container
+        st.chat_message("user").markdown(prompt)
+
+        # Add user message to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
+
+        # create msg to user
+        response = f"Echo: {prompt}"
+
+        with st.chat_message("assistant"):
+            st.markdown(response)
+
+        # Add assistant response to chat history
+        st.session_state.messages.append({"role": "assistant", "content": response})
